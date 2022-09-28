1cb7e8f3d81090aa3502cc16b3df02434de619dd
Neo <hacknorris@tutanota.com>
2022-09-28 21:20:36 +0200

    innit

---
+++ generator.py
@@ -0,0 +1,25 @@
+# console blog generator
+# very wip
+
+"""
+console.log("text","style")
+"""
+html = """
+<html>
+<head></head>
+<body>
+<script>
+
+</script>
+</body>
+</html>
+"""
+
+action = input("what do you want do to? (create) blog , (add) post , (remove) post or (edit) post ?")
+match action:
+  case create:
+  case add:
+  case remove:
+  case edit:
+
+url = ""
