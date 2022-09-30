fa4072903c4aaf6dbf10f7072aabae63b648c6c5
Neo <hacknorris@tutanota.com>
2022-09-30 18:54:33 +0200

    smaller edits

--- generator.py
+++ generator.py
@@ -4,22 +4,32 @@
 """
 console.log("text","style")
 """
+
+#initial html
 html = """
 <html>
 <head></head>
 <body>
-<script>
-
+<script src="index.js">
 </script>
 </body>
 </html>
 """
 
+#initial js
+js = """
+
+"""
+#commands
 action = input("what do you want do to? (create) blog , (add) post , (remove) post or (edit) post ?")
 match action:
-  case create:
+  case create: 
+    f = open("index.html", "a")
+    f.write(html)
+    f.close()
+    g = open("index.js", "a")
+    g.write(js)
+    g.close()
   case add:
   case remove:
   case edit:
-
-url = ""
